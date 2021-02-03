5fe8946d65cf5c050d90bf7b36b40d9ee87acfeb
LucasFlaquer <lucas.flaquer@gmail.com>
2021-02-03 20:53:19 -0300

    added github corner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import LogoVertical from '../../assets/logo-vertical.svg'
 import FetchUser from '../../components/FetchUser';
+import GithubCorner from '../../components/GithubCorner';
 import './styles.scss';
 
 const Home: React.FC = () => {
@@ -10,6 +11,7 @@ const Home: React.FC = () => {
         <img src={LogoVertical} alt="gihub Search"/>
         <FetchUser/>
       </div>
+      <GithubCorner address="https://github.com/LucasFlaquer/github-search" primary="#8752CC"/>
     </div>
   )
 }
